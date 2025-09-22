74318bb7a7056553ac2ec7db8b8d4c8530459b75
nrflorendo-creator <nrflorendo@amaes.edu.ph>
2025-09-22 15:39:52 +0800

    changed confirm to dialog.confirm

--- src/FileCabinet/SuiteScripts/AMA 2.0 ITG/itg_check_series_cs.js
+++ src/FileCabinet/SuiteScripts/AMA 2.0 ITG/itg_check_series_cs.js
@@ -2,130 +2,7 @@
  * @NApiVersion 2.1
  * @NScriptType ClientScript
  */
-define(["N/query"], (query) => {
-  /**
-   * Function to be executed after page is initialized.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
-   *
-   * @since 2015.2
-   */
-  const pageInit = (scriptContext) => {};
-
-  /**
-   * Function to be executed when field is changed.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.sublistId - Sublist name
-   * @param {string} scriptContext.fieldId - Field name
-   * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-   * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-   *
-   * @since 2015.2
-   */
-  const fieldChanged = (scriptContext) => {};
-
-  /**
-   * Function to be executed when field is slaved.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.sublistId - Sublist name
-   * @param {string} scriptContext.fieldId - Field name
-   *
-   * @since 2015.2
-   */
-  const postSourcing = (scriptContext) => {};
-
-  /**
-   * Function to be executed after sublist is inserted, removed, or edited.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.sublistId - Sublist name
-   *
-   * @since 2015.2
-   */
-  const sublistChanged = (scriptContext) => {};
-
-  /**
-   * Function to be executed after line is selected.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.sublistId - Sublist name
-   *
-   * @since 2015.2
-   */
-  const lineInit = (scriptContext) => {};
-
-  /**
-   * Validation function to be executed when field is changed.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.sublistId - Sublist name
-   * @param {string} scriptContext.fieldId - Field name
-   * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-   * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-   *
-   * @returns {boolean} Return true if field is valid
-   *
-   * @since 2015.2
-   */
-  const validateField = (scriptContext) => {};
-
-  /**
-   * Validation function to be executed when sublist line is committed.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.sublistId - Sublist name
-   *
-   * @returns {boolean} Return true if sublist line is valid
-   *
-   * @since 2015.2
-   */
-  const validateLine = (scriptContext) => {};
-
-  /**
-   * Validation function to be executed when sublist line is inserted.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.sublistId - Sublist name
-   *
-   * @returns {boolean} Return true if sublist line is valid
-   *
-   * @since 2015.2
-   */
-  const validateInsert = (scriptContext) => {};
-
-  /**
-   * Validation function to be executed when record is deleted.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @param {string} scriptContext.sublistId - Sublist name
-   *
-   * @returns {boolean} Return true if sublist line is valid
-   *
-   * @since 2015.2
-   */
-  const validateDelete = (scriptContext) => {};
-
-  /**
-   * Validation function to be executed when record is saved.
-   *
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.currentRecord - Current form record
-   * @returns {boolean} Return true if record is valid
-   *
-   * @since 2015.2
-   */
+define(["N/query", "N/ui/dialog"], (query, dialog) => {
   const saveRecord = (scriptContext) => {
     const currRec = scriptContext.currentRecord;
     let blSave = true;
@@ -155,15 +32,22 @@ define(["N/query"], (query) => {
         console.log("objData", objData);
 
         if (objData) {
-          const stMessage = `The combination of Bank: "${objData.bank}", Payment Method: "${objData.method}", and User: "${objData.user}" is already in use.\n\nWould you like to open the existing record?`;
+          const stMessage = `The combination of:<br>- Bank: "${objData.bank}"<br>- Payment Method: "${objData.method}"<br>- User: "${objData.user}"<br>is already in use.<br><br>Would you like to open the existing record?`;
 
-          if (confirm(stMessage)) {
-            const baseUrl = window.location.origin;
-            window.open(
-              `${baseUrl}/app/common/custom/custrecordentry.nl?rectype=${inType}&id=${objData.id}`,
-              "_blank"
-            );
-          }
+          dialog
+            .confirm({
+              title: "Duplicate Combination Found",
+              message: stMessage,
+            })
+            .then(function (result) {
+              if (result) {
+                const baseUrl = window.location.origin;
+                window.open(
+                  `${baseUrl}/app/common/custom/custrecordentry.nl?rectype=${inType}&id=${objData.id}`,
+                  "_blank"
+                );
+              }
+            });
 
           blSave = false;
         }
@@ -173,7 +57,5 @@ define(["N/query"], (query) => {
     return blSave;
   };
 
-  return {
-    saveRecord,
-  };
+  return { saveRecord };
 });
