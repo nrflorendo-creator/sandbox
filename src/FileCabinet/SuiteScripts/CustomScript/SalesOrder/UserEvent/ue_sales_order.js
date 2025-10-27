205a1795dd5dde850f4aba0c658555463d2e7404
nrflorendo-creator <nrflorendo@amaes.edu.ph>
2025-10-27 13:26:34 +0800

    clean up

--- src/FileCabinet/SuiteScripts/CustomScript/SalesOrder/UserEvent/ue_sales_order.js
+++ src/FileCabinet/SuiteScripts/CustomScript/SalesOrder/UserEvent/ue_sales_order.js
@@ -6,51 +6,19 @@ define([
   "../../Library/lib_btn_remove.js",
   "../../Library/lib_pdc_information.js",
 ], (remove, pdcInformation) => {
-  /**
-   * Defines the function definition that is executed before record is loaded.
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.newRecord - New record
-   * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-   * @param {Form} scriptContext.form - Current form
-   * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
-   * @since 2015.2
-   */
   const beforeLoad = (scriptContext) => {
-    const newRec = scriptContext.newRecord;
-    const form = scriptContext.form;
-
     if (scriptContext.type === scriptContext.UserEventType.VIEW) {
       remove.btnRemove({
-        newRec: newRec,
-        form: form,
+        newRec: scriptContext.newRecord,
+        form: scriptContext.form,
       });
 
       pdcInformation.viewList({
-        newRec: newRec,
-        form: form,
+        newRec: scriptContext.newRecord,
+        form: scriptContext.form,
       });
     }
   };
 
-  /**
-   * Defines the function definition that is executed before record is submitted.
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.newRecord - New record
-   * @param {Record} scriptContext.oldRecord - Old record
-   * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-   * @since 2015.2
-   */
-  const beforeSubmit = (scriptContext) => {};
-
-  /**
-   * Defines the function definition that is executed after record is submitted.
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.newRecord - New record
-   * @param {Record} scriptContext.oldRecord - Old record
-   * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-   * @since 2015.2
-   */
-  const afterSubmit = (scriptContext) => {};
-
-  return { beforeLoad, beforeSubmit, afterSubmit };
+  return { beforeLoad };
 });
