8674083b7ecc748400f87f85404505b0994ec99b
nrflorendo-creator <nrflorendo@amaes.edu.ph>
2025-10-29 16:56:39 +0800

    remove comments

--- src/FileCabinet/SuiteScripts/CustomScript/Payment/UserEvent/ue_payment.js
+++ src/FileCabinet/SuiteScripts/CustomScript/Payment/UserEvent/ue_payment.js
@@ -3,44 +3,12 @@
  * @NScriptType UserEventScript
  */
 define(["../Library/lib_payment.js"], (lib) => {
-  /**
-   * Defines the function definition that is executed before record is loaded.
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.newRecord - New record
-   * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-   * @param {Form} scriptContext.form - Current form
-   * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
-   * @since 2015.2
-   */
-  const beforeLoad = (scriptContext) => {};
-
-  /**
-   * Defines the function definition that is executed before record is submitted.
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.newRecord - New record
-   * @param {Record} scriptContext.oldRecord - Old record
-   * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-   * @since 2015.2
-   */
-  const beforeSubmit = (scriptContext) => {};
-
-  /**
-   * Defines the function definition that is executed after record is submitted.
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.newRecord - New record
-   * @param {Record} scriptContext.oldRecord - Old record
-   * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-   * @since 2015.2
-   */
   const afterSubmit = (scriptContext) => {
-    const newRec = scriptContext.newRecord;
-    const type = scriptContext.type;
-
     lib.updatPDC({
-      newRec: newRec,
-      type: type,
+      newRec: scriptContext.newRecord,
+      type: scriptContext.type,
     });
   };
 
-  return { beforeLoad, beforeSubmit, afterSubmit };
+  return { afterSubmit };
 });
